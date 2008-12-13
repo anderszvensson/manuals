07f9887382e0c288de800c02fedd776bf0679b4d
Jaromil <jaromil@dyne.org>
2008-12-13 16:01:26 +0100

    start at multilanguage localised build system (not yet ready, some problems with including images...)

--- dynebolic/images/convert_for_html.sh
+++ dynebolic/images/convert_for_html.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
-mkdir -p html/images
+lang=$1
+target=$2
+
+case $target in
+    html)
+	outdir="$lang/html/images"
+	;;
+    pdf)
+	outdir="$lang/images"
+	;;
+    *)
+	echo "ERROR in image/convert.sh - must specify a target, i.e: convert.sh EN html"
+	exit 0
+	;;
+esac
+	
+mkdir -p $outdir
 
 for i in `ls images/*.png`; do
   file="`basename $i | cut -d. -f1`"
   echo "converting $file to jpeg"
-  convert $i "html/images/${file}"
+  convert $i "$outdir/${file}"
 done
 
-echo "images prepared in html/images"
+echo "images prepared in $outdir"
