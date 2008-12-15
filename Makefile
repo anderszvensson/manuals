7b2b81bc385197a898d69bfb9a71ba0e84345394
Jaromil <jaromil@dyne.org>
2008-12-15 06:02:16 +0100

    cleanup fixes

---
+++ Makefile
@@ -0,0 +1,7 @@
+
+all:
+	# enter a directory and use make in there
+
+clean:
+	make -C dynebolic clean
+	make -C freej clean
--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # configure the language of the manual
 # all available translated chapters will be completed
 
-man-language ?= EN
+man-language ?= ES
 
