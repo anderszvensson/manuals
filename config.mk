484129f7bf494478218c6cd73d53ec93a2e00e29
Jaromil <jaromil@dyne.org>
2008-12-16 01:39:43 +0100

    example scripts in freej manual build system for examples and more cleanups

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # configure the language of the manual
 # all available translated chapters will be completed
 
-man-language ?= ES
+man-language ?= EN
 
--- freej/collate.sh
+++ freej/collate.sh
@@ -6,10 +6,36 @@
 # get which language we're compiling
 lang=$1
 
+if ! [ -x $lang ]; then
+    echo "There is no translation of this manual in [$lang]"
+    exit 1
+fi
+
 name=freej-manual
 
+
+# collate the example scripts for language bindings
+
+for l in ruby python; do
+    rm -f $lang/$l.sgml
+    cat $lang/${l}_intro.sgml > $lang/$l.sgml
+    cat $lang/examples.sgml | awk -v lang=$l '
+BEGIN   { tmpl = 0 }
+/^%/    { print "<screen>"
+          split($1, file, "%");
+          cmd = sprintf("cat %s/%s.src", lang, file[2]); 
+          system(cmd)
+          print "</screen>"
+          tmpl = 1 }
+        { if(tmpl==0) print $0
+          else tmpl = 0 }
+'                     >> "$lang/$l.sgml"
+    echo "</chapter>" >> "$lang/$l.sgml"
+done
+
+
 # list all chapter entities for the selected translation
-entities=`ls $lang/*.sgml| grep -v '$(name)'| grep -v 'authors.sgml'`
+entities=`ls $lang/*.sgml| grep -v '$(name)'| grep -v 'authors.sgml' | grep -v '_intro.sgml$'`
 
 # set the known chapters and their order
 ordered="
